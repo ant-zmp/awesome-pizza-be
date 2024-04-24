86e1764c26fbe9f778a2155150937ec6fff90dda
ok_zampo <a.zampino.dev@outlook.com>
2024-04-24 12:04:38 +0200

    First commit

---
+++ src/main/java/com/portfolio/awesomepizzabe/AwesomePizzaBeApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.awesomepizzabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomePizzaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomePizzaBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/awesomepizzabe/AwesomePizzaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.awesomepizzabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomePizzaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
